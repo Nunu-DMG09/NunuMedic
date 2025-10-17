03fac1968d9df8ee25eb0ef369f22e48d6fd0240
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-17 12:20:31 -0500

    update json

--- backend/src/routes/categorias.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getCategorias, addCategoria } from "../controllers/categoria.controller.js";
-
-const router = express.Router();
-
-router.get("/", getCategorias);
-router.post("/", addCategoria);
-
-export default router;
