96a903f29148af8b989ad71789b40b8f3866f41a
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-19 17:37:06 -0500

    ensure express can parse URL-encoded data

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,7 @@ dotenv.config();
 const app = express();
 app.use(corsMiddleware);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // health
 app.get("/", (req, res) => {
