3989f64359b49c78ed2b81b6cdac7387cf2d1239
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-20 12:44:44 -0500

    fix: update POST route for creating venta to use '/create' endpoint

--- backend/src/routes/venta.route.js
+++ backend/src/routes/venta.route.js
@@ -3,7 +3,7 @@ import { createVenta, getVentaById } from '../controllers/venta.controller.js';
 
 const router = express.Router();
 
-router.post('/', createVenta);
+router.post('/create', createVenta);
 router.get('/:id', getVentaById);
 
 export default router;
