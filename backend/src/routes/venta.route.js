c3f6dd0aed7a390eb20c352af4aa4d8a1ff1f401
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-21 09:42:51 -0500

    add route to retrieve all ventas in venta.route.js

--- backend/src/routes/venta.route.js
+++ backend/src/routes/venta.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createVenta, getVentaById } from '../controllers/venta.controller.js';
+import { createVenta, getVentaById, getAllVentas } from '../controllers/venta.controller.js';
 
 const router = express.Router();
 
 router.post('/create', createVenta);
 router.get('/:id', getVentaById);
+router.get('/', getAllVentas);
+
 
 export default router;
