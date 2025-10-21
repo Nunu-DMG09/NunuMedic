c83827c11321770ba043f23257736fa170506860
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-21 13:07:26 -0500

    add route to list all stock movements

--- backend/src/routes/movimiento_stock.route.js
+++ backend/src/routes/movimiento_stock.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createMovimiento, getMovimientosByProducto } from '../controllers/movimiento_stock.controller.js';
+import { createMovimiento, getAllMovimientos, getMovimientosByProducto } from '../controllers/movimiento_stock.controller.js';
 
 const router = express.Router();
 
 router.post('/', createMovimiento);
+router.get('/listar', getAllMovimientos);
 router.get('/producto/:id', getMovimientosByProducto);
 
 export default router;
