a9cf2972c9d3c6f8b56e18bab659e56ea3e6c422
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-22 23:12:13 -0500

    add knexfile configuration for database connection and migrations

---
+++ backend/knexfile.cjs
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'nunumed',
+    multipleStatements: true
+  },
+  migrations: {
+    directory: './migrations'
+  },
+  seeds: {
+    directory: './seeds'
+  }
+};
