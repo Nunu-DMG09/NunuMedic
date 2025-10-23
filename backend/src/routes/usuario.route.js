c2f3b4e8fd14036d2c8de6984b6dfb140f51bbbd
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-22 23:13:56 -0500

    add route to list all users

--- backend/src/routes/usuario.route.js
+++ backend/src/routes/usuario.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getUsuarioById, createUsuario } from '../controllers/usuario.controller.js';
+import { getUsuarioById, createUsuario, getAllUsuarios } from '../controllers/usuario.controller.js';
 
 const router = express.Router();
 
+router.get('/listar', getAllUsuarios);
 router.get('/:id', getUsuarioById);
 router.post('/', createUsuario);
 
