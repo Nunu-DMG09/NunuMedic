c020ea1df0eaf318ff578f80d08385c921fb86a0
Nunu-DMG09 <davidmesta09@gmail.com>
2025-11-23 15:12:18 -0500

    Refactor usuario.route.js to include user role, password update, and delete functions

--- backend/src/routes/usuario.route.js
+++ backend/src/routes/usuario.route.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { getUsuarioById, createUsuario, getAllUsuarios } from '../controllers/usuario.controller.js';
+import { getUsuarioById, createUsuario, getAllUsuarios, updateUsuarioRol, updateUsuarioClave, deleteUsuario } from '../controllers/usuario.controller.js';
 
 const router = express.Router();
 
 router.get('/listar', getAllUsuarios);
-router.get('/:id', getUsuarioById);
 router.post('/', createUsuario);
+
+// rutas para actualizar rol, clave y eliminar
+router.put('/:id/rol', updateUsuarioRol);
+router.put('/:id/clave', updateUsuarioClave);
+router.delete('/:id', deleteUsuario);
+
+router.get('/:id', getUsuarioById);
 
 export default router;
