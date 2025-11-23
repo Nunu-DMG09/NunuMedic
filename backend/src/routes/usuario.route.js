0c84a7ea16df9d0fdeb79ae6ef3dcc931fbbef63
Nunu-DMG09 <davidmesta09@gmail.com>
2025-11-23 15:18:19 -0500

    Add route for updating user status in usuario.route.js

--- backend/src/routes/usuario.route.js
+++ backend/src/routes/usuario.route.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { getUsuarioById, createUsuario, getAllUsuarios, updateUsuarioRol, updateUsuarioClave, deleteUsuario } from '../controllers/usuario.controller.js';
+import { getUsuarioById, createUsuario, getAllUsuarios, updateUsuarioRol, updateUsuarioClave, deleteUsuario, updateUsuarioEstado } from '../controllers/usuario.controller.js';
 
 const router = express.Router();
 
 router.get('/listar', getAllUsuarios);
 router.post('/', createUsuario);
 
-// rutas para actualizar rol, clave y eliminar
+// rutas para actualizar rol, clave, estado y eliminar
 router.put('/:id/rol', updateUsuarioRol);
 router.put('/:id/clave', updateUsuarioClave);
+router.put('/:id/estado', updateUsuarioEstado);
 router.delete('/:id', deleteUsuario);
 
 router.get('/:id', getUsuarioById);
